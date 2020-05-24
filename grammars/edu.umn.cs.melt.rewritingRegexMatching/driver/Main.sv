a16dc92ac8168b7332002ebf9caf2f093e0a6074
Lucas Kramer <krame505@umn.edu>
2020-05-23 20:40:36 -0500

    Performance test files

--- grammars/edu.umn.cs.melt.rewritingRegexMatching/driver/Main.sv
+++ grammars/edu.umn.cs.melt.rewritingRegexMatching/driver/Main.sv
@@ -16,18 +16,23 @@ IOVal<Integer> ::= args::[String] ioIn::IO
 {
   local fileName :: String = head(args);
   local result::IOMonad<Integer> = do (bindIO, returnIO) {
-    if length(args) != 2 then {
-      printM("Usage: java -jar regex.jar [regex] [string]\n");
+    if length(args) < 1 || length(args) > 2 then {
+      printM("Usage: java -jar regex.jar [regex] (string)\nIf a string is not provided, input is read from stdin.\n");
       return 2;
     } else {
       regex::String = head(args);
-      text::String = head(tail(args));
+      text::String <-
+        if null(tail(args))
+        then readFileM("/dev/stdin")
+        else returnIO(head(tail(args)));
       result :: ParseResult<Regex_c> = parse(regex, fileName);
       if !result.parseSuccess then {
         printM(result.parseErrors ++ "\n");
         return 3;
       } else {
         ast::Regex = result.parseTree.ast;
+        --printM(hackUnparse(stringToChars(text)) ++ "\n");
+        --printM(hackUnparse(ast) ++ "\n");
         if matches(ast, text) then {
           printM("Match success\n");
           return 0;
