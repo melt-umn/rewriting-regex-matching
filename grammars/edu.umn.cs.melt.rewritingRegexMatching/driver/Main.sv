562e6bb05b9817fc20997e750efae4a44a8d4ab9
unironically <lbvac230@gmail.com>
2022-01-12 15:29:01 -0600

    Refactored IO types and functions

--- grammars/edu.umn.cs.melt.rewritingRegexMatching/driver/Main.sv
+++ grammars/edu.umn.cs.melt.rewritingRegexMatching/driver/Main.sv
@@ -11,32 +11,32 @@ parser parse::Regex_c {
 }
 
 function main
-IOVal<Integer> ::= args::[String] ioIn::IO
+IOVal<Integer> ::= args::[String] ioIn::IOToken
 {
   local fileName :: String = head(args);
-  local result::IOMonad<Integer> = do {
+  local result::IO<Integer> = do {
     if length(args) < 1 || length(args) > 2 then do {
-      printM("Usage: java -jar regex.jar [regex] (string)\nIf a string is not provided, input is read from stdin.\n");
+      print("Usage: java -jar regex.jar [regex] (string)\nIf a string is not provided, input is read from stdin.\n");
       return 2;
     } else do {
       let regex::String = head(args);
       text::String <-
         if null(tail(args))
-        then readFileM("/dev/stdin")
+        then readFile("/dev/stdin")
         else pure(head(tail(args)));
       let result :: ParseResult<Regex_c> = parse(regex, fileName);
       if !result.parseSuccess then do {
-        printM(result.parseErrors ++ "\n");
+        print(result.parseErrors ++ "\n");
         return 3;
       } else do {
         let ast::Regex = result.parseTree.ast;
-        --printM(hackUnparse(stringToChars(text)) ++ "\n");
-        --printM(hackUnparse(ast) ++ "\n");
+        --print(hackUnparse(stringToChars(text)) ++ "\n");
+        --print(hackUnparse(ast) ++ "\n");
         if matches(ast, text) then do {
-          printM("Match success\n");
+          print("Match success\n");
           return 0;
         } else do {
-          printM("Match failure\n");
+          print("Match failure\n");
           return 1;
         };
       };
